9ff77fab56a70e10101f063a8083346cf1b5a199
Kenan <mckenzie.kenan@yahoo.com>
2022-09-11 12:24:42 -0400

    Added prompts and validation

--- script.js
+++ script.js
@@ -6,8 +6,21 @@ var numbers = [0,1,2,3,4,5,6,7,8,9]
 
 function generatePassword() {
     //1.Ask for password length, if outside 8-128 return to prompt
+    passwordLength = prompt("Please select a password length between 8-128 characters.") 
+    if (passwordLength < 8 || passwordLength > 128) {
+    alert("Invalid response, please try again")
+    return prompt("Please select a password length between 8-128 characters.")
+    } 
     //2.Prompts log responses of arrays to include -> new array
+    includeUpper = confirm("Would you like to include uppercase letters?")    
+    includeLower = confirm("Would you like to include lowercase letters?")   
+    includeSymbols = confirm("Would you like to include special characters?")  
+    includeNumbers = confirm("Would you like to include numbers?") 
     //3.Validate that at least one criteria was selected
+    if (!includeUpper, !includeLower, !includeSymbols, !includeNumbers) {
+        alert("Please choose to include at least one type of character.")
+        return generatePassword()
+    }
     //4.Either gen. password from one combined array or get random character from each and push them
     
 }
