b58d6a047a8710b4037e9df1627fc9b09304ca33
Kenan <mckenzie.kenan@yahoo.com>
2022-09-11 12:18:40 -0400

    outline steps

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ var specialCharacters = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var numbers = [0,1,2,3,4,5,6,7,8,9]
 
+function generatePassword() {
+    //1.Ask for password length, if outside 8-128 return to prompt
+    //2.Prompts log responses of arrays to include -> new array
+    //3.Validate that at least one criteria was selected
+    //4.Either gen. password from one combined array or get random character from each and push them
+    
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
